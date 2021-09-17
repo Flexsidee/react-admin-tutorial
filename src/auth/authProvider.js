f3dc031326061e40b49fb1a94c2caf9438906cba
flexsidee <flexsidee@gmail.com>
2021-09-17 11:46:22 +0100

    latest update

--- src/auth/authProvider.js
+++ src/auth/authProvider.js
@@ -1,3 +1,4 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
     //called when the user attempts to log in
     login: ({username}) =>{
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import {Card, CardContent, CardHeader} from '@material-ui/core';
 
-export default () => (
+const Dashboard = () => (
    <Card>
        <CardHeader title="Welcome to the administration" />
        <CardContent>I've been waiting for tonigh tonigh, make we drink up while we reminisce</CardContent>
    </Card>
 );
+export default Dashboard;
