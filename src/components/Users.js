76c06596d8da6d56211299a57690a4a683b0f8e3
flexsidee <flexsidee@gmail.com>
2021-09-17 12:42:46 +0100

    latest update

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { List, Datagrid, TextField, EmailField} from 'react-admin';
+import { List, Datagrid, TextField, EmailField, SimpleList} from 'react-admin';
 import MyUrlField from './MyUrlField';
+import { useMediaQuery } from '@material-ui/core';
 
+const UserList = props => {
+    const isSmall = useMediaQuery(device => device.breakpoints.down('sm'));
 
-const UserList = props => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="name" />
-            <TextField source="username" />
-            <EmailField source="email" />
-            <TextField source="address.street" />
-            <TextField source="phone" />
-            <MyUrlField source="website" />
-            <TextField source="company.name" />
-        </Datagrid>
-    </List>
-)
+    return(
+        <List {...props}>
+            {isSmall ? (
+                <SimpleList 
+                    primaryText={record => record.name}
+                    secondaryText={record => record.email}
+                    tertiaryText={record => record.address.street}
+                />
+            ) : (
+            <Datagrid rowClick="edit">
+                <TextField source="id" />
+                <TextField source="name" />
+                <TextField source="username" />
+                <EmailField source="email" />
+                <TextField source="address.street" />
+                <TextField source="phone" />
+                <MyUrlField source="website" />
+                <TextField source="company.name" />
+            </Datagrid>
+            )}
+        </List>
+    );
+}
 
 export default UserList;
