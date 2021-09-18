d9396b0b4da546931339d4d07d39b04a4532c3bc
flexsidee <flexsidee@gmail.com>
2021-09-18 22:50:42 +0100

    latest update

--- src/App.js
+++ src/App.js
@@ -7,11 +7,18 @@ import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import Dashboard from './components/Dashboard';
 import authProvider from './auth/authProvider';
+import { createTheme } from '@material-ui/core/styles';
 
 const dataProvider=jsonServerProvider('https://jsonplaceholder.typicode.com');
 
+const theme=createTheme({
+  palette: {
+    type: 'light',
+  },
+});
+
 const App = () =>(
-  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} theme={theme} authProvider={authProvider} dataProvider={dataProvider} title="React Admin Tutorial">
     <Resource name="users" list={UserList} icon={UserIcon} />
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
   </Admin>
